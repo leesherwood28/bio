4a721e7fcab26260bf5e390df80d30ebee202198
Lee Sherwood <leesherwood28@googlemail.com>
2022-09-10 15:37:57 +0100

    Setting up game

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+const withTM = require('next-transpile-modules')(['three']);
+module.exports = withTM(nextConfig);
