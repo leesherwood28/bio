ff029e38b62eb6dd7ab2fb260438394ff393cd85
Lee Sherwood <leesherwood28@googlemail.com>
2022-09-10 15:52:42 +0100

    Setting up initial game

--- components/Game.tsx
+++ components/Game.tsx
@@ -1,3 +1,16 @@
-export default function Game({}) {
-    
-}
+import { Canvas } from '@react-three/fiber';
+
+const Game: React.FunctionComponent = () => {
+  return (
+    <Canvas>
+      <mesh>
+        <boxGeometry />
+        <meshStandardMaterial />
+        <ambientLight intensity={0.1} />
+        <directionalLight color='red' position={[0, 0, 5]} />
+      </mesh>
+    </Canvas>
+  );
+};
+
+export default Game;
