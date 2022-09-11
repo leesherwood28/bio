cfec69e21f8eb720208760f5c5b05289875a3aac
Lee Sherwood <leesherwood28@googlemail.com>
2022-09-11 15:15:17 +0100

    Setting up player

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,20 +1,19 @@
 import { Canvas } from '@react-three/fiber';
-import Ground from './Ground';
+import { Suspense } from 'react';
 import Orbit from './dev/Orbit';
-import CameraHelper from './dev/CameraHelper';
-import { createRef } from 'react';
+import Ground from './Ground';
 import Player from './Player';
 
 const Game: React.FunctionComponent = () => {
   return (
     <Canvas camera={{ position: [0, 4, 10] }}>
       <Ground />
-      <Player />
-      <ambientLight intensity={0.3} />
-      <directionalLight color='blue' position={[1, 1, 5]} />
+      <Suspense fallback={null}>
+        <Player />
+      </Suspense>
+      <ambientLight intensity={0.5} />
       {/* Remove */}
-      {/* <Orbit /> */}
-      {/* <CameraHelper /> */}
+      <Orbit />
     </Canvas>
   );
 };
--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,10 +1,28 @@
-import { useGLTF } from '@react-three/drei';
-import React from 'react';
+import { useGLTF, useAnimations } from '@react-three/drei';
+import React, { useEffect, useLayoutEffect, useRef } from 'react';
 import { GLTF } from 'three/examples/jsm/loaders/GLTFLoader';
 
 const Player: React.FunctionComponent = () => {
-  const { scene } = useGLTF('/player/scene.gltf') as GLTF;
-  return <primitive object={scene} />;
+  const group = useRef();
+  const { scene, animations } = useGLTF(
+    '/player/scene.gltf',
+    'https://www.gstatic.com/draco/versioned/decoders/1.4.0/'
+  ) as GLTF;
+  const { actions } = useAnimations(animations, group);
+
+  useLayoutEffect(() => {
+    actions['Root|Walk']?.play();
+  }, [actions]);
+  return (
+    <group
+      ref={group}
+      position={[0, 3, 5]}
+      scale={[0.01, 0.01, 0.01]}
+      rotation={[0, Math.PI, 0]}
+    >
+      <primitive object={scene} />
+    </group>
+  );
 };
 
 export default Player;
