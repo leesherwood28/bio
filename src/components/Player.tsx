a52f86c05808f534ac11afcbe6414fb71a69d57c
Lee Sherwood <leesherwood28@googlemail.com>
2022-09-15 21:18:29 +0100

    Making player walk forward

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,21 +1,31 @@
 import { useGLTF, useAnimations } from '@react-three/drei';
+import { useFrame } from '@react-three/fiber';
 import React, { useEffect, useLayoutEffect, useRef } from 'react';
+import { Group } from 'three';
 import { GLTF } from 'three/examples/jsm/loaders/GLTFLoader';
 
 const Player: React.FunctionComponent = () => {
-  const group = useRef();
+  const playerRef = useRef<Group>(null);
   const { scene, animations } = useGLTF(
     '/player/scene.gltf',
     'https://www.gstatic.com/draco/versioned/decoders/1.4.0/'
   ) as GLTF;
-  const { actions } = useAnimations(animations, group);
+  const { actions } = useAnimations(animations, playerRef);
 
   useLayoutEffect(() => {
     actions['Root|Walk']?.play();
   }, [actions]);
+
+  useFrame(() => {
+    if (!playerRef.current) {
+      return;
+    }
+    playerRef.current.position.z -= 0.01;
+  });
+
   return (
     <group
-      ref={group}
+      ref={playerRef}
       position={[0, 3, 5]}
       scale={[0.01, 0.01, 0.01]}
       rotation={[0, Math.PI, 0]}
