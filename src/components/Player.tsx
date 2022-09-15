c80c365aa11d8500bd34a08f33ef0c7655e56cf3
Lee Sherwood <leesherwood28@googlemail.com>
2022-09-15 23:23:40 +0100

    Initial player setup

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,11 +1,11 @@
 import { useGLTF, useAnimations } from '@react-three/drei';
 import { useFrame } from '@react-three/fiber';
 import React, { useEffect, useLayoutEffect, useRef } from 'react';
-import { Group, Vector2, Vector3 } from 'three';
+import { Euler, Group, Matrix4, Vector2, Vector3 } from 'three';
 import { GLTF } from 'three/examples/jsm/loaders/GLTFLoader';
 import { ControllerState, useGameStore } from '../state/game-store';
 
-const PLAYER_MOVE_SPEED = 0.01;
+const PLAYER_MOVE_SPEED = 0.04;
 
 const convertBooleanToValue = (bool?: Boolean) => (bool ? 1 : 0);
 
@@ -41,19 +41,33 @@ const Player: React.FunctionComponent = () => {
     }
     const movementVector = convertControlsIntoMovementVector(controls);
 
+    // TODO Use state machine instead and fix for different
+    // refresh rates
+    if (movementVector.lengthSq() !== 0) {
+      actions['Root|Idle 01 ']?.stop();
+      actions['Root|Run  02 ']?.play();
+      player.current.lookAt(
+        player.current.position
+          .clone()
+          .add(movementVector.clone().multiplyScalar(-1))
+      );
+    } else {
+      actions['Root|Run  02 ']?.stop();
+      actions['Root|Idle 01 ']?.play();
+    }
+
     player.current.position.x += movementVector.x;
     player.current.position.y += movementVector.y;
     player.current.position.z += movementVector.z;
   });
 
   return (
-    <group
-      ref={player}
-      position={[0, 3, 5]}
-      scale={[0.01, 0.01, 0.01]}
-      rotation={[0, Math.PI, 0]}
-    >
-      <primitive object={scene} />
+    <group ref={player} position={[0, 3, 5]}>
+      <primitive
+        object={scene}
+        rotation={[0, Math.PI, 0]}
+        scale={[0.01, 0.01, 0.01]}
+      />
     </group>
   );
 };
