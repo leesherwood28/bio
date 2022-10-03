a20811ce02c40b1684b457fbb40672687f218632
Lee Sherwood <leesherwood28@googlemail.com>
2022-10-03 18:58:00 +0100

    Making ground smaller

--- src/components/Ground.tsx
+++ src/components/Ground.tsx
@@ -1,7 +1,7 @@
 const Ground: React.FunctionComponent = () => {
   return (
     <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]} position={[0, 0, 0]}>
-      <planeBufferGeometry args={[1000, 1000]} />
+      <planeBufferGeometry args={[100, 100]} />
       <shadowMaterial color='#171717' />
       <meshStandardMaterial color='green' />
     </mesh>
