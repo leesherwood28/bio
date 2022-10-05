76d100ba847889835f4ff3fcc7c3c9032118dcb1
Lee Sherwood <leesherwood28@googlemail.com>
2022-10-05 02:20:47 +0100

    removed dome

--- src/components/GlassDome.tsx
+++ src/components/GlassDome.tsx
@@ -9,7 +9,7 @@ import {
 export function GlassDome() {
   return (
     <mesh position={[0, 1, 0]}>
-      <sphereGeometry args={[50, 20, 20]} />
+      {/* <sphereGeometry args={[50, 20, 20]} /> */}
     </mesh>
   );
 }
