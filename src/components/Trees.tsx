9251edadb18cf81892cf57a990ec87d70fdbfc7f
Lee Sherwood <leesherwood28@googlemail.com>
2022-10-07 00:04:59 +0100

    Setting up trees

--- src/components/Trees.tsx
+++ src/components/Trees.tsx
@@ -5,150 +5,132 @@ import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
 import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader';
 
 import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import {
   AdditiveBlending,
   DoubleSide,
+  Euler,
+  Group,
   MaterialLoader,
   ObjectLoader,
   ShaderChunk,
   Texture,
+  Vector3,
 } from 'three';
 import * as maath from 'maath';
+import { generateUUID } from 'three/src/math/MathUtils';
 
-const MODELS = [];
+// interface ModelData {
+//   numberAvailable: number;
+//   radius: number;
+// }
 
-const TEXTURES = [
-  'BirchTree_Bark_Normal',
-  'BirchTree_Bark',
-  'BirchTree_Leaves',
-  'Flowers',
-  'Grass',
-  'Leaves_BW',
-  'MapleTree_Bark_Normal',
-  'MapleTree_Bark',
-  'MapleTree_Leaves_BW',
-  'MapleTree_Leaves',
-  'NormalTree_Bark_Normal',
-  'NormalTree_Bark',
-  'NormalTree_Leaves',
-  'PalmTree_Leaves',
-  'PalmTree_Trunk_Normal',
-  'PalmTree_Trunk',
-  'PineTree_Bark_Normal',
-  'PineTree_Bark',
-  'PineTree_Leaves',
-  'Rocks',
-];
+// interface ModelDef {
+//   numberAvailable: number;
+//   radius: number;
+//   name: string;
+// }
 
-const NORMALS = [
-  'BirchTree_Bark',
-  'MapleTree_Bark',
-  'NormalTree_Bark',
-  'PalmTree_Trunk',
-  'PineTree_Bark',
-];
+// const Models: ModelDef[] = [
+//      {
+//         name: 'Willow_Autumn',
+//         numberAvailable: 5,
+//         radius: 5,
+//       },
+//       BirchTree_Autumn:{
+//         name: 'BirchTree_Autumn',
+//         numberAvailable: 5,
+//         radius: 5,
+//       },
+//       Bush: {
+//         numberAvailable: 5,
+//         radius: 2,
+//       },
+//       CommonTree_Autumn: {
+//         numberAvailable: 5,
+//         radius: 2,
+//       },
+//       Corn: {
+//         numberAvailable: 2,
+//         radius: 0,
+//       },
+//       Rock: {
+//         numberAvailable: 7,
+//         radius: 2,
+//       },
+//       Rock_Moss: {
+//         numberAvailable: 7,
+//         radius: 2,
+//       },
+// ]
 
 // Models used with love from https://quaternius.com/;
 
-const useTexturesMap = (
-  textures: string[],
-  pathFn: (t: string) => string
-): Map<string, Texture> => {
-  const loadedTextures = useTexture(textures.map((t) => pathFn(t)));
-  const map = new Map<string, Texture>();
-  for (let i = 0; i < textures.length; i++) {
-    map.set(textures[i], loadedTextures[i]);
-  }
-  return map;
-};
+interface Model {
+  radius: number;
+  item: Group;
+  id: string;
+  position: Vector3;
+}
 
 const Trees: React.FunctionComponent = () => {
-  const fbx = useFBX('/foilage/BirchTree_1.fbx');
-  return <primitive scale={0.025} object={fbx} />;
-
-  const { scene, nodes, materials } = useGLTF(
-    '/foilage/BirchTree_1.gltf'
-  ) as any;
-
-  //   const materials = useLoader(MTLLoader, '/foilage/BirchTree_1.mtl');
-  //   const object = useLoader(OBJLoader, '/foilage/BirchTree_1.obj', (loader) => {
-  //     materials.preload();
-  //     (loader as any).setMaterials(materials);
-  //   }) as any;
-
-  const textures = useTexturesMap(
-    TEXTURES,
-    (t) => `/foilage/textures/${t}.png`
-  );
-  const normals = useTexturesMap(
-    NORMALS,
-    (t) => `/foilage/textures/${t}_Normal.png`
-  );
-
-  useEffect(() => {
-    scene.traverse((t: any) => {
-      if (!t.isMesh) {
-        return;
-      }
-      t.side = DoubleSide;
-      t.material.side = DoubleSide;
-      t.material.transparent = true;
-
-      if (textures.has(t.material.name)) {
-        t.material.map = textures.get(t.material.name);
-        t.material.needsUpdate = true;
-      }
-      if (normals.has(t.material.name)) {
-        t.material.normalMap = normals.get(t.material.name);
-        t.material.needsUpdate = true;
-      }
-    });
-  }, [scene, textures]);
+  const MAX_DISTANCE = 100;
 
   return (
-    <group dispose={null}>
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.Cube004.geometry}
-        material={materials.BirchTree_Bark}
+    <>
+      <RandomFoliageSet
+        path={'Willow_Autumn'}
+        number={20}
+        available={5}
+        maxDistance={MAX_DISTANCE}
       />
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.Cube004_1.geometry}
-        material={materials.BirchTree_Leaves}
-      />
-    </group>
+    </>
   );
+};
 
-  return <primitive object={scene}></primitive>;
+interface FoilageSetParams {
+  path: string;
+  number: number;
+  available: number;
+  maxDistance: number;
+}
+
+const RandomFoliageSet: React.FunctionComponent<FoilageSetParams> = ({
+  path,
+  available,
+  number,
+  maxDistance,
+}) => {
+  const [foilage] = useState(() => {
+    return Array.from({ length: number }).map((_, i) => {
+      const position = new Vector3(Math.random() * maxDistance).applyEuler(
+        new Euler(0, Math.random() * 2 * Math.PI, 0)
+      );
+
+      const randomIndex = Math.floor(Math.random() * available) + 1;
+
+      const model: Model = {
+        id: generateUUID(),
+        item: useFBX(`/foilage/${path}_${randomIndex}.fbx`),
+        position: position,
+        radius: 1,
+      };
+      return model;
+    });
+  });
+
+  return (
+    <>
+      {foilage.map((f) => (
+        <primitive
+          scale={0.015}
+          key={f.id}
+          object={f.item}
+          position={f.position}
+        />
+      ))}
+    </>
+  );
 };
 
 export default Trees;
-
-// function Stars() {
-//   const ref = useRef<any>();
-//   const [sphere] = useState(() =>
-//     maath.random.inSphere(new Float32Array(5000), { radius: 1.5 })
-//   );
-//   return (
-//     <group rotation={[0, 0, Math.PI / 4]}>
-//       <Points
-//         ref={ref}
-//         positions={sphere as any}
-//         stride={3}
-//         frustumCulled={false}
-//       >
-//         <PointMaterial
-//           transparent
-//           color='#ffa0e0'
-//           size={0.005}
-//           sizeAttenuation={true}
-//           depthWrite={false}
-//         />
-//       </Points>
-//     </group>
-//   );
-// }
