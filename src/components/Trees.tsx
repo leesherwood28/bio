c0caf93e2bef2cc8bf0ea608bcf42ec9da17e2cd
Lee Sherwood <leesherwood28@googlemail.com>
2022-10-06 18:11:06 +0100

    Adding normals to textures

--- src/components/Trees.tsx
+++ src/components/Trees.tsx
@@ -3,6 +3,7 @@ import { useLoader } from '@react-three/fiber';
 import { GLTF } from 'three/examples/jsm/loaders/GLTFLoader';
 import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader';
 import { useEffect } from 'react';
+import { Texture } from 'three';
 
 const TEXTURES = [
   'BirchTree_Bark_Normal',
@@ -27,22 +28,51 @@ const TEXTURES = [
   'Rocks',
 ];
 
+const NORMALS = [
+  'BirchTree_Bark',
+  'MapleTree_Bark',
+  'NormalTree_Bark',
+  'PalmTree_Trunk',
+  'PineTree_Bark',
+];
+
+const useTexturesMap = (
+  textures: string[],
+  pathFn: (t: string) => string
+): Map<string, Texture> => {
+  const loadedTextures = useTexture(textures.map((t) => pathFn(t)));
+  const map = new Map<string, Texture>();
+  for (let i = 0; i < textures.length; i++) {
+    map.set(textures[i], loadedTextures[i]);
+  }
+  return map;
+};
+
 const Trees: React.FunctionComponent = () => {
   const { scene } = useGLTF('/foilage/BirchTree_1.gltf') as any;
-  const textures = useTexture(
-    TEXTURES.map((t) => `/foilage/textures/${t}.png`)
+  const textures = useTexturesMap(
+    TEXTURES,
+    (t) => `/foilage/textures/${t}.png`
+  );
+  const normals = useTexturesMap(
+    NORMALS,
+    (t) => `/foilage/textures/${t}_Normal.png`
   );
 
   useEffect(() => {
     textures.forEach((t) => (t.flipY = false));
 
     scene.traverse((t: any) => {
-      if (t.isMesh) {
-        const textureIndex = TEXTURES.findIndex((x) => x === t.material.name);
-        if (textureIndex !== -1) {
-          t.material.map = textures[textureIndex];
-          t.material.needsUpdate = true;
-        }
+      if (!t.isMesh) {
+        return;
+      }
+      if (textures.has(t.material.name)) {
+        t.material.map = textures.get(t.material.name);
+        t.material.needsUpdate = true;
+      }
+      if (normals.has(t.material.name)) {
+        t.material.normalMap = normals.get(t.material.name);
+        t.material.needsUpdate = true;
       }
     });
   }, [scene, textures]);
