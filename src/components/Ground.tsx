2de3998cd200d365ecf4181aa979981ff2469e12
Lee Sherwood <leesherwood28@googlemail.com>
2022-10-07 15:15:57 +0100

    Removing material

--- src/components/Ground.tsx
+++ src/components/Ground.tsx
@@ -4,7 +4,6 @@ const Ground: React.FunctionComponent = () => {
   return (
     <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]} position={[0, 0, 0]}>
       <circleGeometry args={[WORLD_RADIUS, 50, 50]} />
-      <shadowMaterial color='#171717' />
       <meshStandardMaterial color='#146600' />
     </mesh>
   );
