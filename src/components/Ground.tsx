bca23b72b3299ca327469ff8d5103469453217c8
Lee Sherwood <leesherwood28@googlemail.com>
2022-10-12 18:31:12 +0100

    Setting up grass

--- src/components/Ground.tsx
+++ src/components/Ground.tsx
@@ -1,10 +1,18 @@
+import { useTexture } from '@react-three/drei';
 import { WORLD_RADIUS } from '../contants/world-radius.const';
+import { RepeatWrapping, NearestFilter } from 'three';
 
 const Ground: React.FunctionComponent = () => {
+  const grassTexture = useTexture('/ground/grass.jpg');
+
+  grassTexture.wrapS = RepeatWrapping;
+  grassTexture.wrapT = RepeatWrapping;
+  grassTexture.repeat.set(100, 100);
+  grassTexture.magFilter = NearestFilter;
   return (
     <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]} position={[0, 0, 0]}>
       <circleGeometry args={[WORLD_RADIUS, 50, 50]} />
-      <meshStandardMaterial color='#146600' />
+      <meshBasicMaterial map={grassTexture} />
     </mesh>
   );
 };
