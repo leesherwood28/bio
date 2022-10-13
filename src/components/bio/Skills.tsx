b528a2546f1c9687a9a983d40671ab41233ef82d
Lee Sherwood <leesherwood28@googlemail.com>
2022-10-13 12:51:01 +0100

    Adding front end technologies

--- src/components/bio/Skills.tsx
+++ src/components/bio/Skills.tsx
@@ -1,5 +1,56 @@
+import Image from 'next/image';
+
 const Skills: React.FunctionComponent = () => {
-  return null;
+  return (
+    <div className='text-center flex flex-col items-center gap-2'>
+      <h2>Web Technologies</h2>
+      <p data-scroll-reveal className='text-sm'>
+        I have good experience with the following web technologies:
+      </p>
+      <SkillChipCollection>
+        <SkillChip text='Angular' asset='angular.svg' />
+        <SkillChip text='Typescript' asset='typescript.svg' />
+        <SkillChip text='Akita' asset='akita.svg' />
+        <SkillChip text='NGXS' asset='ngxs.png' />
+        <SkillChip text='D3' asset='d3.svg' />
+        <SkillChip text='Ionic' asset='ionic.svg' />
+        <SkillChip text='Capacitor' asset='capacitor.png' />
+        <SkillChip text='SASS' asset='sass.svg' />
+        <SkillChip text='Tailwind' asset='tailwind.svg' />
+        <SkillChip text='Webpack' asset='webpack.svg' />
+        <SkillChip text='Parcel' asset='parcel.png' />
+        <SkillChip text='Leaflet' asset='leaflet.svg' />
+        <SkillChip text='NodeJS' asset='nodejs.png' />
+        <SkillChip text='NX' asset='nx.png' />
+      </SkillChipCollection>
+    </div>
+  );
+};
+
+interface SkillChipCollectionParams {
+  children?: React.ReactNode;
+}
+
+const SkillChipCollection: React.FunctionComponent<
+  SkillChipCollectionParams
+> = ({ children }) => {
+  return <div className='grid grid-cols-2'>{children}</div>;
+};
+
+interface SkillShipParams {
+  text: string;
+  asset: string;
+}
+const SkillChip: React.FunctionComponent<SkillShipParams> = ({
+  text,
+  asset: assetPath,
+}) => {
+  return (
+    <div className='flex items-center bg-white text-black rounded-full px-2'>
+      <Image className='w-4 h-4 mr-2' src={'/icons/' + assetPath} />
+      {text}
+    </div>
+  );
 };
 
 export default Skills;
