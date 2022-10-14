eddfd76324b76a53315e6b8ea541ad38c193f2bc
Lee Sherwood <leesherwood28@googlemail.com>
2022-10-14 14:42:46 +0100

    Setting up three element

--- src/components/three-constructs/HtmlThreeElement.tsx
+++ src/components/three-constructs/HtmlThreeElement.tsx
@@ -1,18 +1,37 @@
 import { CSS3DObject } from 'three/examples/jsm/renderers/CSS3DRenderer';
 import { extend, Object3DNode, ReactThreeFiber } from '@react-three/fiber';
+import { NoBlending } from 'three';
 
 extend({ CSS3DObject });
 
-declare module '@react-three/fiber' {
-  interface ThreeElements {
-    css3DObject: Object3DNode<CSS3DObject, typeof CSS3DObject>;
-  }
+interface HtmlThreeElementParams {
+  width: number;
+  height: number;
 }
 
-const HtmlThreeElement: React.FunctionComponent = () => {
+const HtmlThreeElement: React.FunctionComponent<HtmlThreeElementParams> = ({
+  width,
+  height,
+}) => {
+  const element = document.createElement('button');
+  element.style.width = width + 'px';
+  element.style.height = height + 'px';
+  element.style.opacity = '0.999';
+  element.style.boxSizing = 'border-box';
+  element.textContent = 'testing';
+
   return (
     <object3D>
-      <css3DObject></css3DObject>
+      <css3DObject element={element}></css3DObject>
+
+      <mesh>
+        <boxGeometry args={[width, height]} />
+        <meshPhongMaterial
+          opacity={0.15}
+          blending={NoBlending}
+          color={'white'}
+        />
+      </mesh>
     </object3D>
   );
 };
