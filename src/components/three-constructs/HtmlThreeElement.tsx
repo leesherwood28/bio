607b8b91825b565cf84493ce91b164e11bc9cf28
Lee Sherwood <leesherwood28@googlemail.com>
2022-10-14 17:04:27 +0100

    Setting up three elements

--- src/components/three-constructs/HtmlThreeElement.tsx
+++ src/components/three-constructs/HtmlThreeElement.tsx
@@ -1,5 +1,6 @@
 import { CSS3DObject } from 'three/examples/jsm/renderers/CSS3DRenderer';
 import {
+  createRoot,
   extend,
   Object3DNode,
   Object3DProps,
@@ -14,7 +15,7 @@ import {
   NoBlending,
   Object3D,
 } from 'three';
-import { useEffect, useRef } from 'react';
+import { useEffect, useMemo, useRef } from 'react';
 import { isNil } from '../../functions/is-nil.fn';
 
 extend({ CSS3DObject });
@@ -22,26 +23,36 @@ extend({ CSS3DObject });
 interface HtmlThreeElementParams {
   width: number;
   height: number;
+  children: React.ReactNode;
 }
 
 const HtmlThreeElement: React.FunctionComponent<
   HtmlThreeElementParams & Object3DProps
-> = ({ width, height, ...params }) => {
+> = ({ width, height, children, ...params }) => {
   const objectRef = useRef<Object3D<Event>>(null);
 
   const { scene } = useThree();
+  const renderer = useMemo(() => {
+    const container = Object.assign(document.createElement('div'), {});
+    return createRoot(container);
+  }, []);
+
+  useEffect(() => {
+    renderer.render(children);
+  });
 
   useEffect(() => {
     if (isNil(objectRef.current)) {
       return;
     }
+
     const element = document.createElement('div');
     const innerEl = document.createElement('div');
     element.append(innerEl);
-    element.style.transform = 'scale3d(0.00625, 0.00625, 0.00625)';
+    innerEl.style.transform = 'scale3d(0.00625, 0.00625, 0.00625)';
     element.style.width = width + 'px';
     element.style.height = height + 'px';
-    element.style.backgroundColor = 'red';
+    // element.style.backgroundColor = 'red';
     innerEl.textContent = 'hello';
 
     const css3d = new CSS3DObject(element);
@@ -53,14 +64,16 @@ const HtmlThreeElement: React.FunctionComponent<
   }, [objectRef, scene]);
 
   return (
-    <object3D ref={objectRef} {...params}>
-      {/* <cSS3DObject attach='css3dObject'></cSS3DObject> */}
+    <>
+      <object3D ref={objectRef} {...params}>
+        {/* <cSS3DObject attach='css3dObject'></cSS3DObject> */}
 
-      <mesh>
-        <boxGeometry args={[width, height]} />
-        <meshBasicMaterial opacity={0.4} transparent color={'white'} />
-      </mesh>
-    </object3D>
+        <mesh>
+          <boxGeometry args={[width, height]} />
+          <meshBasicMaterial opacity={0.4} transparent color={'white'} />
+        </mesh>
+      </object3D>
+    </>
   );
 };
 
