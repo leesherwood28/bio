8142fca23f9a19ec0f405f1f44ed84720938e8ec
Lee Sherwood <leesherwood28@googlemail.com>
2022-10-17 16:19:55 +0100

    Setting up skills

--- src/components/bio/Skills.tsx
+++ src/components/bio/Skills.tsx
@@ -3,10 +3,8 @@ import Image from 'next/image';
 const Skills: React.FunctionComponent = () => {
   return (
     <div className='text-center flex flex-col items-center gap-2'>
-      <h2 className='text-base'>Web Technologies</h2>
-      <p data-scroll-reveal className=''>
-        I have good experience with the following web technologies:
-      </p>
+      <h2 className='text-lg'>Web Technologies</h2>
+      <p>I have good experience with the following web technologies:</p>
       <SkillChipCollection>
         <SkillChip text='Angular' asset='angular.svg' />
         <SkillChip text='Typescript' asset='typescript.svg' />
@@ -23,6 +21,34 @@ const Skills: React.FunctionComponent = () => {
         <SkillChip text='NodeJS' asset='nodejs.png' />
         <SkillChip text='NX' asset='nx.png' />
       </SkillChipCollection>
+      <h2 className='text-lg'>Backend Technologies</h2>
+      <p>I have good experience with the following backend technologies:</p>
+      <SkillChipCollection>
+        <SkillChip text='Firebase' asset='firebase.svg' />
+        <SkillChip text='.NET Core' asset='netcore.svg' />
+        <SkillChip text='.NET Framework' asset='netframework.svg' />
+        <SkillChip text='SQL Server' asset='sqlserver.png' />
+      </SkillChipCollection>
+      <h2 className='text-lg'>Tools and Services</h2>
+      <p>I also have good experience with the following tools and services:</p>
+      <SkillChipCollection>
+        <SkillChip text='Git' asset='git.svg' />
+        <SkillChip text='Azure' asset='azure.svg' />
+        <SkillChip text='Azure Devops' asset='azure-devops.svg' />
+        <SkillChip text='Github' asset='github.png' />
+        <SkillChip text='Gitlab' asset='gitlab.svg' />
+        <SkillChip text='npm' asset='npm.svg' />
+      </SkillChipCollection>
+      <h2>Practices</h2>
+      <p>I have experience with the following practises:</p>
+      <ul>
+        <li>Object Oriented Programming</li>
+        <li>Functional Programming</li>
+        <li>Unit Testing</li>
+        <li>Agile</li>
+        <li>Scrum</li>
+        <li>SAFE</li>
+      </ul>
     </div>
   );
 };
