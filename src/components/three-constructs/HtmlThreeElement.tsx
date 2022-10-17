0215e8067d259604b11987bcb537e2c940e957dc
Lee Sherwood <leesherwood28@googlemail.com>
2022-10-17 13:16:52 +0100

    Improving three element

--- src/components/three-constructs/HtmlThreeElement.tsx
+++ src/components/three-constructs/HtmlThreeElement.tsx
@@ -1,79 +1,47 @@
+import { extend, GroupProps } from '@react-three/fiber';
+import { useCallback, useEffect, useMemo, useState } from 'react';
+import { createRoot } from 'react-dom/client';
+import { NoBlending } from 'three';
 import { CSS3DObject } from 'three/examples/jsm/renderers/CSS3DRenderer';
-import {
-  extend,
-  GroupProps,
-  Object3DNode,
-  Object3DProps,
-  ReactThreeFiber,
-  useThree,
-} from '@react-three/fiber';
-import {
-  BoxGeometry,
-  Color,
-  Mesh,
-  MeshBasicMaterial,
-  NoBlending,
-  Object3D,
-} from 'three';
-import { useEffect, useMemo, useRef, useState, useLayoutEffect } from 'react';
-import { createRoot, Root } from 'react-dom/client';
-
-import { isNil } from '../../functions/is-nil.fn';
 
 extend({ CSS3DObject });
 
 interface HtmlThreeElementParams {
-  width: number;
-  height: number;
   children: React.ReactNode;
+  scaler: number;
 }
 
 const HtmlThreeElement: React.FunctionComponent<
   HtmlThreeElementParams & GroupProps
-> = ({ width, height, children, ...params }) => {
+> = ({ children, scaler = 160, ...params }) => {
   const rootEl = useMemo(() => document.createElement('div'), []);
   const root = useMemo(() => createRoot(rootEl), []);
+  const [{ width, height }, setWidthHeight] = useState({ width: 0, height: 0 });
+  const resizeObserver = useMemo(() => {
+    return new ResizeObserver((entries) => {
+      const { width, height } = entries[0].contentRect;
+      // Remove single pixel to ensure complete overlap
+      // of rendered element to the mesh
+      setWidthHeight({ width: width - 1, height: height - 1 });
+    });
+  }, []);
 
   useEffect(() => {
     root.render(children);
+    resizeObserver.observe(rootEl);
+    return () => resizeObserver.unobserve(rootEl);
   }, [root, children]);
-  // const objectRef = useRef<Object3D<Event>>(null);
-  // const { scene } = useThree();
-
-  // useLayoutEffect(() => {
-  //   if (isNil(objectRef.current)) {
-  //     return;
-  //   }
-
-  //   el.style.width = width + 'px';
-  //   el.style.height = height + 'px';
-  //   const css3d = new CSS3DObject(el);
-  //   css3d.scale = new Array(3).fill(1/ 160);
-  //   objectRef.current.add(css3d);
-
-  //   const child = document.createElement('div');
-  //   el.append(child);
-  //   // child.style.transform = 'scale3d(0.00625, 0.00625, 0.00625)';
-  //   // // child.style.height = 160 * height + 'px';
-  //   // child.style.width = 160 * width + 'px';
-  //   // child.style.transformOrigin = 'left top';
-  //   root.current = createRoot(child);
-
-  //   return () => {
-  //     root.current?.unmount();
-  //   };
-  // }, [objectRef]);
 
   return (
     <>
       <group {...params}>
         <cSS3DObject
           args={[rootEl]}
-          scale={[1 / 160, 1 / 160, 1 / 160]}
+          scale={[1 / scaler, 1 / scaler, 1 / scaler]}
         ></cSS3DObject>
 
-        <mesh>
-          <planeGeometry args={[width, height]} />
+        <mesh scale={[width / scaler, height / scaler, 1]}>
+          <planeGeometry />
           <meshPhongMaterial
             color={0x111111}
             blending={NoBlending}
