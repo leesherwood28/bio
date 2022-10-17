cb9b015f4a3c80e297e94c23bc4eff79ff753d15
Lee Sherwood <leesherwood28@googlemail.com>
2022-10-17 13:01:21 +0100

    Cleaning renderer

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,12 +1,5 @@
 import { Canvas } from '@react-three/fiber';
-import {
-  Camera,
-  Event,
-  Object3D,
-  PCFSoftShadowMap,
-  Scene,
-  WebGLRenderer,
-} from 'three';
+import { PCFSoftShadowMap, WebGLRenderer } from 'three';
 import Orbit from './dev/Orbit';
 import BlackHole from './environment/BlackHole';
 import Foliage from './environment/Foliage';
@@ -19,45 +12,9 @@ import GameCamera from './game/GameCamera';
 import JoystickInput from './input/JoystickInput';
 import KeyboardInput from './input/KeyboardInput';
 import Player from './player/Player';
+import { CSS3DRenderer } from 'three/examples/jsm/renderers/CSS3DRenderer';
 
-import {} from '@react-three/fiber';
-import CSS3DSceneRenderer from './three-constructs/CSS3DSceneRenderer';
-import { Renderer } from '@react-three/fiber/dist/declarations/src/core/store';
-import {
-  CSS3DObject,
-  CSS3DRenderer,
-} from 'three/examples/jsm/renderers/CSS3DRenderer';
-import { WebGLRendererParameters } from 'three';
-
-class DualRenderer implements Renderer {
-  private readonly webGlRenderer = new WebGLRenderer({ canvas: this.canvas });
-  private readonly cssRenderer = new CSS3DRenderer();
-
-  constructor(private canvas: HTMLCanvasElement) {
-    this.cssRenderer.domElement.style.position = 'absolute';
-    this.cssRenderer.domElement.style.top = '0';
-    this.cssRenderer.setSize(window.innerWidth, window.innerHeight);
-    document
-      .querySelector('#game-container')
-      ?.prepend(this.cssRenderer.domElement);
-  }
-
-  render(scene: Scene, camera: Camera) {
-    this.webGlRenderer.render(scene, camera);
-    this.cssRenderer.render(scene, camera);
-  }
-
-  setPixelRatio(ratio: number) {
-    this.webGlRenderer.setPixelRatio(ratio);
-  }
-
-  setSize(width: number, height: number, updateStyle: boolean) {
-    this.webGlRenderer.setSize(width, height, updateStyle);
-    this.cssRenderer.setSize(width, height);
-  }
-}
-
-const DualRendererThree = function (canvas: HTMLCanvasElement) {
+const DualRenderer = function (canvas: HTMLCanvasElement) {
   //@ts-ignore
   WebGLRenderer.apply(this, [{ canvas }]);
   //@ts-ignore
@@ -68,10 +25,8 @@ const DualRendererThree = function (canvas: HTMLCanvasElement) {
   this.cssRenderer.domElement.style.top = '0';
   //@ts-ignore
   this.cssRenderer.setSize(window.innerWidth, window.innerHeight);
-  document
-    .querySelector('#game-container')
-    //@ts-ignore
-    ?.prepend(this.cssRenderer.domElement);
+  //@ts-ignore
+  canvas.parentElement?.prepend(this.cssRenderer.domElement);
 
   //@ts-ignore
   const currentRender = this.render.bind(this);
@@ -93,42 +48,8 @@ const DualRendererThree = function (canvas: HTMLCanvasElement) {
     this.cssRenderer.setSize(width, height);
   };
 };
-DualRendererThree.prototype = WebGLRenderer.prototype;
-DualRendererThree.prototype.constructor = DualRendererThree;
-
-class DualRendererTwo extends WebGLRenderer {
-  private readonly cssRenderer = new CSS3DRenderer();
-
-  constructor(private canvas: HTMLCanvasElement) {
-    super({ canvas });
-    console.log('here wtf');
-
-    this.cssRenderer.domElement.style.position = 'absolute';
-    this.cssRenderer.domElement.style.top = '0';
-    this.cssRenderer.setSize(window.innerWidth, window.innerHeight);
-    console.log('here wtf like');
-
-    console.log(this.render);
-    //@ts-ignore
-    delete this.render;
-    document
-      .querySelector('#game-container')
-      ?.prepend(this.cssRenderer.domElement);
-
-    Object.defineProperty(this, 'render', (scene: Scene, camera: Camera) => {});
-  }
-  override render(scene: Scene, camera: Camera): void {
-    console.log('here');
-    // super.render(scene, camera);
-    this.cssRenderer.render(scene, camera);
-  }
-
-  override setSize(width: number, height: number, updateStyle: boolean) {
-    console.log('here 2');
-    super.setSize(width, height, updateStyle);
-    this.cssRenderer.setSize(width, height);
-  }
-}
+DualRenderer.prototype = WebGLRenderer.prototype;
+DualRenderer.prototype.constructor = DualRenderer;
 
 const Game: React.FunctionComponent = () => {
   return (
@@ -138,7 +59,7 @@ const Game: React.FunctionComponent = () => {
         camera={{ far: 5000 }}
         shadows={{ type: PCFSoftShadowMap }}
         //@ts-ignore
-        gl={(canvas) => new DualRendererThree(canvas)}
+        gl={(canvas) => new DualRenderer(canvas)}
       >
         <Physics />
         <Player />
