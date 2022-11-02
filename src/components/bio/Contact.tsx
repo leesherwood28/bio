5bcbfa46e5b7c81f129b912f0e1468794f9429fe
Lee Sherwood <leesherwood28@googlemail.com>
2022-11-02 16:11:23 +0000

    Adding contact

--- src/components/bio/Contact.tsx
+++ src/components/bio/Contact.tsx
@@ -6,21 +6,27 @@ const Contact: React.FunctionComponent = () => {
         If you wish to contact me, my contact details are below. I have also
         linked my Github if you are interested in my code!
       </p>
-      <div className='grid grid-cols-2 gap-y-6 gap-x-4 text-sm'>
-        <a
-          className='link'
-          href='https://www.linkedin.com/in/lee-sherwood-b93807ba'
-        >
-          <i className='fab fa-linkedin mr-4' aria-hidden='true'></i>LinkedIn
-        </a>
-        <a className='link' href='https://github.com/leesherwood28'>
-          <i className='fab fa-github mr-4' aria-hidden='true'></i>Github
-        </a>
-        <a className='link col-span-2' href='mailto:leesherwood1@gmail.com'>
-          <i className='fa fa-envelope mr-4' aria-hidden='true'></i>
-          leesherwood1@gmail.com
-        </a>
-      </div>
+      <ol className='list-disc'>
+        <li>
+          <a
+            className='link'
+            target='_blank'
+            href='https://www.linkedin.com/in/lee-sherwood-b93807ba'
+          >
+            LinkedIn
+          </a>
+        </li>
+        <li>
+          <a className='link' href='https://github.com/leesherwood28'>
+            Github
+          </a>
+        </li>
+        <li>
+          <a className='link ' href='mailto:leesherwood1@gmail.com'>
+            leesherwood1@gmail.com
+          </a>
+        </li>
+      </ol>
     </div>
   );
 };
