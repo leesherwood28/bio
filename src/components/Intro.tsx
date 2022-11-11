0194a44fe6698a42e701787e4422498dbe48d1de
Lee Sherwood <leesherwood28@googlemail.com>
2022-11-11 02:20:48 +0000

    Removing unused code

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,11 +1,4 @@
-import {
-  useTransition,
-  useSpring,
-  useChain,
-  config,
-  animated,
-  useSpringRef,
-} from '@react-spring/web';
+import { animated, useSpring } from '@react-spring/web';
 
 const Intro: React.FunctionComponent = () => {
   return (
