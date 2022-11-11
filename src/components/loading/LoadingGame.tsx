196450b5b73a676b0642060d44f35db82c789bda
Lee Sherwood <leesherwood28@googlemail.com>
2022-11-11 18:52:55 +0000

    Using spring for loading game

--- src/components/loading/LoadingGame.tsx
+++ src/components/loading/LoadingGame.tsx
@@ -1,21 +1,27 @@
+import { useSpring, animated } from '@react-spring/web';
 import { useProgress } from '@react-three/drei';
 
 const LoadingGame: React.FunctionComponent = () => {
-  const { active, progress } = useProgress();
-  console.log(progress);
+  const { active, progress, item } = useProgress();
+
+  const animatedBarStyles = useSpring({
+    from: { scaleX: 0 },
+    to: { scaleX: progress / 100 },
+  });
+  console.log({ item, progress });
   return (
     <div className='absolute bottom-4 py-2 flex flex-col items-center gap-2 w-full'>
       <span className='text-white text-xl'>
         Loading: {progress.toFixed(0)} %
       </span>
-      <div className='h-4 w-full'>
-        <div
+      <div className='h-2 w-3/5'>
+        <animated.div
           style={{
             transformOrigin: 'center left',
-            transform: `scaleX(${progress / 100})`,
+            ...animatedBarStyles,
           }}
-          className='h-full w-full bg-orange-500'
-        ></div>
+          className='h-full bg-orange-500'
+        ></animated.div>
       </div>
     </div>
   );
