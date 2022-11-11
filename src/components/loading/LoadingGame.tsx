92a3d8d6a763aef3342d59934491d831fe43fe3d
Lee Sherwood <leesherwood28@googlemail.com>
2022-11-11 17:06:03 +0000

    Setting up loading bar

--- src/components/loading/LoadingGame.tsx
+++ src/components/loading/LoadingGame.tsx
@@ -1,7 +1,19 @@
+import { useProgress } from '@react-three/drei';
+
 const LoadingGame: React.FunctionComponent = () => {
+  const { active, progress } = useProgress();
   return (
-    <div className='absolute bottom-4 text-white -translate-x-1/2 left-1/2 text-xl'>
-      Loading Game Files
+    <div className='absolute bottom-4 py-2 flex flex-col items-center gap-2 w-full'>
+      <span>Loading: {progress.toFixed(0)} %</span>
+      <div className='h-4 w-full'>
+        <div
+          style={{
+            transformOrigin: 'center left',
+            transform: `scaleX(${progress / 100})`,
+          }}
+          className='h-full w-full bg-orange-500'
+        ></div>
+      </div>
     </div>
   );
 };
