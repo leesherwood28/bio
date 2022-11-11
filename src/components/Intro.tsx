8ee11d4af3b4ec56d0c171f27f79367ff8f557fb
Lee Sherwood <leesherwood28@googlemail.com>
2022-11-11 02:20:39 +0000

    Setting up intro animation

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,70 +1,121 @@
+import {
+  useTransition,
+  useSpring,
+  useChain,
+  config,
+  animated,
+  useSpringRef,
+} from '@react-spring/web';
+
 const Intro: React.FunctionComponent = () => {
   return (
     <div
       className='
-w-full h-full absolute
-  bg-black
-  grid grid-cols-1 grid-rows-2
-  md:grid-rows-3 md:grid-cols-3
-  place-items-center
-  text-white
-'
+        w-full h-full absolute
+        bg-black
+        grid grid-cols-1 grid-rows-2
+        md:grid-rows-3 md:grid-cols-3
+        place-items-center
+        text-white'
     >
       <div
         className='
-    font-anton
-    text-white-smoke text-8xl
-    place-self-stretch
-    flex
-    items-center
-    justify-center
-    md:row-start-2 md:col-start-2
-  '
+            font-anton
+            text-white-smoke text-8xl
+            place-self-stretch
+            flex
+            items-center
+            justify-center
+            md:row-start-2 md:col-start-2'
       >
-        <span className='text-yellow-600 animate__animated animate__backInLeft'>
-          &lt;
-        </span>
-        <span
-          className='
-      px-8
-      animate__animated animate__bounceIn animate__delay-750ms
-    '
-        >
-          /
-        </span>
-        <span className='text-yellow-600 animate__animated animate__backInRight'>
-          &gt;
-        </span>
+        <Arrow animateDirection='left'>&lt;</Arrow>
+        <Slash />
+        <Arrow animateDirection='right'>&gt;</Arrow>
       </div>
 
       <div
         className='
-    text-white-smoke
-    font-montserrat
-    text-3xl
-    self-start
-    md:place-self-stretch md:row-start-3 md:col-start-3
-  '
+            text-white-smoke
+            font-montserrat
+            text-3xl
+            self-start
+            md:place-self-stretch md:row-start-3 md:col-start-3'
       >
-        <div
-          className='
-      mb-4
-      animate__animated animate__fadeIn animate__delay-1500ms
-    '
-        >
+        <FadeInText className='mb-4' delay={1500}>
           Lee Sherwood
-        </div>
-        <div
-          className='
-      animate__animated animate__fadeIn animate__delay-2500ms
-      italic
-      text-yellow-600 text-2xl
-    '
-        >
+        </FadeInText>
+
+        <FadeInText className='italic text-yellow-600 text-2xl' delay={2500}>
           Web Developer
-        </div>
+        </FadeInText>
       </div>
     </div>
+  );
+};
+
+interface ArrowProps {
+  children: React.ReactNode;
+  animateDirection: 'left' | 'right';
+}
+const Arrow: React.FunctionComponent<ArrowProps> = ({
+  children,
+  animateDirection,
+}) => {
+  const animatedStyles = useSpring({
+    from: {
+      x: animateDirection === 'left' ? -2000 : 2000,
+      scale: 0.7,
+      opacity: 0.7,
+    },
+    to: [{ x: 0 }, { scale: 1, opacity: 1 }],
+  });
+
+  return (
+    <animated.span style={animatedStyles} className='text-yellow-600'>
+      {children}
+    </animated.span>
+  );
+};
+
+const Slash: React.FunctionComponent = () => {
+  const animatedStyles = useSpring({
+    from: {
+      scale: 0.3,
+      opacity: 0,
+    },
+    to: [{ scale: 1, opacity: 1 }],
+    delay: 750,
+    config: { tension: 1000, mass: 2 },
+  });
+  return (
+    <animated.span style={animatedStyles} className='px-8'>
+      /
+    </animated.span>
+  );
+};
+
+interface FadeInTextProps {
+  className: string;
+  children: React.ReactNode;
+  delay: number;
+}
+const FadeInText: React.FunctionComponent<FadeInTextProps> = ({
+  className,
+  children,
+  delay,
+}) => {
+  const animatedStyles = useSpring({
+    from: {
+      opacity: 0,
+    },
+    to: { opacity: 1 },
+    delay,
+  });
+
+  return (
+    <animated.div style={animatedStyles} className={className}>
+      {children}
+    </animated.div>
   );
 };
 
