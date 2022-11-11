861c278286f8fac096ab378a78c9c2e8d4a535e0
Lee Sherwood <leesherwood28@googlemail.com>
2022-11-11 17:23:31 +0000

    Improving font loading

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,9 @@
 import type { NextPage } from 'next';
 import dynamic from 'next/dynamic';
-import Head from 'next/head';
-import Intro from '../src/components/Intro';
-import { useInitialisationStore } from '../src/store/initialisation.store';
 import { Suspense } from 'react';
+import Intro from '../src/components/Intro';
 import LoadingGameFiles from '../src/components/loading/LoadingGameFiles';
+import { useInitialisationStore } from '../src/store/initialisation.store';
 
 const Game = dynamic(() => import('../src/components/Game'), {
   suspense: true,
@@ -17,22 +16,6 @@ const Home: NextPage = () => {
 
   return (
     <div className='w-screen h-screen relative overflow-hidden'>
-      <Head>
-        <title>Lee Sherwood</title>
-        <link rel='preconnect' href='https://fonts.gstatic.com' />
-        <link
-          href='https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap'
-          rel='stylesheet'
-        />
-        <link
-          href='https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400&display=swap'
-          rel='stylesheet'
-        />
-        <link
-          href='https://fonts.googleapis.com/css2?family=Anton&display=swap'
-          rel='stylesheet'
-        />
-      </Head>
       <Intro />
       <Suspense fallback={<LoadingGameFiles />}>
         <Game />
