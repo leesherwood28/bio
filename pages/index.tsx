868a5b66d3ca65eaca6aec57663f7d5f2ba2f18a
Lee Sherwood <leesherwood28@googlemail.com>
2022-11-13 22:33:18 +0000

    Testing game removal

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,17 +4,15 @@ import { Suspense } from 'react';
 import Intro from '../src/components/Intro';
 import LoadingGameFiles from '../src/components/loading/LoadingGameFiles';
 
-const Game = dynamic(() => import('../src/components/Game'), {
-  suspense: true,
-  ssr: false,
-});
+// const Game = dynamic(() => import('../src/components/Game'), {
+//   suspense: true,
+//   ssr: false,
+// });
 
 const Home: NextPage = () => {
   return (
     <div className='w-screen h-screen relative overflow-hidden'>
-      <Suspense fallback={<LoadingGameFiles />}>
-        <Game />
-      </Suspense>
+      <Suspense fallback={<LoadingGameFiles />}>{/* <Game /> */}</Suspense>
       <Intro />
     </div>
   );
