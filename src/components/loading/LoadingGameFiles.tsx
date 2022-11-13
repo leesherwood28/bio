0745cad56a551bdf01318d5c3b6f746b6f354bec
Lee Sherwood <leesherwood28@googlemail.com>
2022-11-13 21:33:44 +0000

    Adding loading game files

--- src/components/loading/LoadingGameFiles.tsx
+++ src/components/loading/LoadingGameFiles.tsx
@@ -1,7 +1,7 @@
 const LoadingGameFiles: React.FunctionComponent = () => {
   return (
-    <div className='absolute bottom-4 text-white -translate-x-1/2 left-1/2 text-xl'>
-      Loading Game Files
+    <div className='absolute bottom-4 text-white -translate-x-1/2 left-1/2 text-xl z-20'>
+      Loading Game Files...
     </div>
   );
 };
