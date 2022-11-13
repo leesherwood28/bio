a9d613074b92dc5c674a9e6720f03dc37433fb62
Lee Sherwood <leesherwood28@googlemail.com>
2022-11-13 22:43:02 +0000

    Adding description

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,20 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 
+const author = 'Lee Sherwood';
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <title>Lee Sherwood</title>
+        <meta charSet='utf-8' />
+        <meta name='language' content='english' />
+        <meta httpEquiv='content-type' content='text/html' />
+        <meta name='author' content={author} />
+        <meta name='designer' content={author} />
+        <meta name='publisher' content={author} />
+        <meta name='description' content={`Lee Sherwood's CV site`} />
       </Head>
       <Component {...pageProps} />
     </>
