e58cf48594cdeb745771b052e4149791a9143aa8
Lee Sherwood <leesherwood28@googlemail.com>
2022-11-14 00:05:56 +0000

    Removing console log

--- src/components/loading/LoadingGameFiles.tsx
+++ src/components/loading/LoadingGameFiles.tsx
@@ -1,5 +1,4 @@
 const LoadingGameFiles: React.FunctionComponent = () => {
-  console.log('here');
   return (
     <div className='absolute bottom-4 text-white -translate-x-1/2 left-1/2 text-xl z-20'>
       Loading Game Files...
